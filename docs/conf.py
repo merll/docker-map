040f3148eb22f3f24badc3cb4551588078fc5fb7
Matthias Erll <matthias@erll.de>
2014-10-23 07:50:54 +0200

    Doc configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -105,9 +105,13 @@ pygments_style = 'sphinx'
 # -- Options for HTML output ----------------------------------------------
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    try:
+        import sphinx_rtd_theme
+    except ImportError:
+        pass  # Ignore, go with default options
+    else:
+        html_theme = 'sphinx_rtd_theme'
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -264,6 +268,6 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+}
