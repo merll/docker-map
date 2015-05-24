merll/docker-map

commit 3f9ada8df387a7ff8fbbda53487c9fb00c1403f5

Revised dependency resolution.

dockermap/map/dep.py
@@ -1,22 +1,66 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from abc import abstractmethod, ABCMeta
-from collections import namedtuple, defaultdict
+from abc import ABCMeta
+from collections import defaultdict
 from six import iteritems, with_metaclass
 
 
-Dependency = namedtuple('Dependency', ('parent', 'dependency_found'))
+class NotInitialized(object):
+    """
+    Utility class to locate nodes in the dependency tree where there is no cached dependency structure yet.
+    """
+    pass
+
+
+class CachedDependency(object):
+    """
+    Caching dependencies along the hierarchy.
+    """
+    __slots__ = ['_parent', '_dependencies']
+
+    def __init__(self, parent, dependencies=NotInitialized):
+        self._parent = parent
+        self._dependencies = dependencies
+
+    def __repr__(self):
+        d = self._dependencies if self._dependencies is not NotInitialized else '<NotInitialized>'
+        return 'CachedDependency(parent={0!r}, dependencies={1!r})'.format(self._parent, d)
+
+    @property
+    def parent(self):
+        """
+        Parent node(s).
+        """
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        self._parent = value
+
+    @property
+    def dependencies(self):
+        """
+        Boolean, list, or other representation of cached dependencies from the parent. Returns ``NotInitialized`` if
+        not cached yet.
+        """
+        return self._dependencies
+
+    @dependencies.setter
+    def dependencies(self, value):
+        self._dependencies = value
+
+
+def _iterate_dependencies(items):
+    if not items:
+        return ()
+    if isinstance(items, dict):
+        return iteritems(items)
+    return items
 
 
 def _dependency_dict(items):
-    if items is None:
-        return {}
-    if isinstance(items, dict):
-        iterator = iteritems(items)
-    else:
-        iterator = items
-    return {item: Dependency(parent, None) for item, parent in iterator}
+    return {item: CachedDependency(parent) for item, parent in _iterate_dependencies(items)}
 
 
 class CircularDependency(Exception):
@@ -30,7 +74,6 @@ class BaseDependencyResolver(with_metaclass(ABCMeta, object)):
     """
     Base class for resolving dependencies of hierarchical nodes. Not each node has to be relevant, and on each level
     parent dependencies can be merged with the current node.
-    In a subclass, at least `merge_dependency` and `check_circular_func` have to be implemented.
 
     :param initial: Optional: Iterable or dictionary in the format `(dependent_item, dependence)`.
     :type initial: iterable
@@ -40,35 +83,25 @@ class BaseDependencyResolver(with_metaclass(ABCMeta, object)):
 
     def merge_dependency(self, item, resolve_parent, parent):
         """
-        Called by `get_dependencies` once on each node. The result is cached for re-occurring checks. This method
-        determines the relevancy (and potentially additional dependencies) of the current node `item`, and how the
-        result is merged with dependencies from the deeper hierarchy. The latter are resolved by calling
-        `resolve_parent(parent)`.
-        By default, this function will always return `False`, as the node relevancy is not defined. It should never
-        return `None`, only empty lists, strings etc.
+        Called by :meth:`~BaseDependencyResolver.get_dependencies` once on each node. The result is cached for
+        re-occurring checks. This method determines the relevancy (and potentially additional dependencies) of the
+        current node `item`, and how the result is merged with dependencies from the deeper hierarchy. The latter are
+        resolved by calling `resolve_parent(parent)`.
+
+        By default, this function will always return `False`, as the node relevancy is not defined.
+
+        This function should, if applicable, also check for potential infinite recursions and in that case raise
+        a :class:`~CircularDependency` exception.
 
         :param item: Current node.
         :param resolve_parent: Function to check on dependencies deeper in the hierarchy.
-        :type resolve_parent: __builtin__.function
+        :type resolve_parent: function
         :param parent: Parent node(s).
-        :return: Result of the dependency merge. May be just boolean, a set, or anything else. Should return `False`
-            if there are no dependencies, not `None`.
+        :return: Result of the dependency merge. May be boolean, a set, or anything else that represents all
+         dependencies.
         :rtype: bool
         """
         return parent is not None and resolve_parent(parent)
-
-    @abstractmethod
-    def check_circular_func(self, start_item):
-        """
-        Function to be implemented by subclasses to check whether nodes are interdependent. Needs to return a function
-        that accepts one argument: the current node. That function should return `True` on a circular dependency,
-        `False` otherwise.
-
-        :param start_item: Node that the dependency check started with.
-        :return: Function that checks if interdependent items have been found.
-        :rtype: __builtin__.function
-        """
-        pass
 
     def get_dependencies(self, item):
         """
@@ -76,21 +109,16 @@ class BaseDependencyResolver(with_metaclass(ABCMeta, object)):
 
         :param item: Node to start the dependency check with.
         :return: The result on merged dependencies down the hierarchy.
-        :raise CircularDependency: If some element in the hierarchy depends on the start node.
         """
         def _get_sub_dependency(sub_item):
             e = self._deps.get(sub_item)
-            if e is not None:
-                if e.dependency_found is None:
-                    if circular_func(e.parent):
-                        raise CircularDependency("Circular dependency found for item '{0}' at '{1}'.".format(item, sub_item))
-                    dep_found = self.merge_dependency(sub_item, _get_sub_dependency, e.parent)
-                    self._deps[sub_item] = Dependency(e.parent, dep_found)
-                    return dep_found
-                return e.dependency_found
-            return False
+            if e is None:
+                return None
 
-        circular_func = self.check_circular_func(item)
+            if e.dependencies is NotInitialized:
+                e.dependencies = self.merge_dependency(sub_item, _get_sub_dependency, e.parent)
+            return e.dependencies
+
         return _get_sub_dependency(item) or ()
 
     def update(self, items):
@@ -109,15 +137,6 @@ class SingleDependencyResolver(with_metaclass(ABCMeta, BaseDependencyResolver)):
     Abstract, partial implementation of a dependency resolver for nodes in a 1:n relationship, i.e. that each node
     depends on exactly one item.
     """
-    def check_circular_func(self, start_item):
-        """
-        Provides the check if the dependence equals the node `start_item`, that the check originally started with.
-
-        :return: Function with argument `item` that checks for `item == start_item`.
-        :rtype: __builtin__.function
-        """
-        return lambda item: item == start_item
-
     def update_backward(self, items):
         """
         Updates the dependencies in the inverse relationship format, i.e. from an iterable or dict that is structured
@@ -128,7 +147,7 @@ class SingleDependencyResolver(with_metaclass(ABCMeta, BaseDependencyResolver)):
         """
         for parent, sub_items in items:
             for si in sub_items:
-                self._deps[si] = Dependency(parent, None)
+                self._deps[si] = CachedDependency(parent)
 
 
 class MultiDependencyResolver(with_metaclass(ABCMeta, BaseDependencyResolver)):
@@ -137,29 +156,20 @@ class MultiDependencyResolver(with_metaclass(ABCMeta, BaseDependencyResolver)):
     depends on one or multiple items.
     """
     def __init__(self, initial=None):
-        self._deps = defaultdict(lambda: Dependency(set(), None), _dependency_dict(initial))
-
-    def check_circular_func(self, start_item):
-        """
-        Provides the check if `start_item` is in any of the dependencies.
-
-        :return: Function with argument `items` that checks for `start_item in items`.
-        :rtype: __builtin__.function
-        """
-        return lambda items: start_item in items
+        self._deps = defaultdict(lambda: CachedDependency(set()), _dependency_dict(initial))
 
     def merge_dependency(self, item, resolve_parent, parents):
         """
-        Performs the same operation as `BaseDependencyResolver.merge_dependency()`, but considering that the node
+        Performs the same operation as :meth:`~BaseDependencyResolver.merge_dependency`, but considering that the node
         `item` may have multiple dependencies `parent`.
 
         :param item: Current node.
         :param resolve_parent: Function to check on dependencies deeper in the hierarchy.
-        :type resolve_parent: __builtin__.function
+        :type resolve_parent: function
         :param parents: Parent nodes.
         :type parents: iterable
-        :return: Result of the dependency merge. May be just boolean, a set, or anything else. Should return `False`
-            if there are no dependencies, not `None`.
+        :return: Result of the dependency merge. May be boolean, a set, or anything else that represents all
+         dependencies.
         :rtype: bool
         """
         return parents is not None and any(resolve_parent(parent) for parent in parents)
@@ -172,9 +182,10 @@ class MultiDependencyResolver(with_metaclass(ABCMeta, BaseDependencyResolver)):
         :param items: Iterable or dictionary in the format `(dependent_item, dependencies)`.
         :type items: iterable
         """
-        for item, parents in iteritems(_dependency_dict(items)):
+        for item, parents in _iterate_dependencies(items):
             dep = self._deps[item]
-            self._deps[item] = Dependency(dep.parent.union(parents.parent), None)
+            dep.parent.update(parents)
+            dep.dependencies = NotInitialized
 
     def update_backward(self, items):
         """
@@ -184,12 +195,7 @@ class MultiDependencyResolver(with_metaclass(ABCMeta, BaseDependencyResolver)):
         :param items: Iterable or dictionary in the format `(item, dependent_items)`.
         :type items: iterable
         """
-        if isinstance(items, dict):
-            iterator = iteritems(items)
-        else:
-            iterator = items
-        for parent, sub_items in iterator:
+        for parent, sub_items in _iterate_dependencies(items):
             for si in sub_items:
                 dep = self._deps[si]
                 dep.parent.add(parent)
-                self._deps[si] = Dependency(dep.parent, None)

dockermap/map/policy/dep.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from ..dep import MultiDependencyResolver
+from itertools import chain
+
+from ..dep import MultiDependencyResolver, CircularDependency
 
 
 class ContainerDependencyResolver(MultiDependencyResolver):
@@ -29,12 +31,18 @@ class ContainerDependencyResolver(MultiDependencyResolver):
         :type parents: iterable
         :return: List of recursively resolved dependencies of this container.
         :rtype: list
+        :raise CircularDependency: If the current element depends on one found deeper in the hierarchy.
         """
-        dep = list(parents)
-        for parent in parents:
-            parent_dep = resolve_parent(parent)
+        dep = []
+        for parent_dep in chain.from_iterable(map(resolve_parent, parents)):
             if parent_dep:
-                dep.extend(set(parent_dep).difference(dep))
+                new_dep = [p for p in parent_dep if p not in dep]
+                dep.extend(new_dep)
+
+        new_dep = [p for p in parents if p not in dep]
+        dep.extend(new_dep)
+        if item in dep:
+            raise CircularDependency("Circular dependency found for item '{0}'.".format(item))
         return dep
 
     def get_container_dependencies(self, map_name, container):

commit 81dedf7681928e8f115848af3e4487ba80f9dfa4

Fix for bug introduced with 3f9ada8, test for validation.

dockermap/map/dep.py
@@ -113,13 +113,13 @@ class BaseDependencyResolver(with_metaclass(ABCMeta, object)):
         def _get_sub_dependency(sub_item):
             e = self._deps.get(sub_item)
             if e is None:
-                return None
+                return ()
 
             if e.dependencies is NotInitialized:
                 e.dependencies = self.merge_dependency(sub_item, _get_sub_dependency, e.parent)
             return e.dependencies
 
-        return _get_sub_dependency(item) or ()
+        return _get_sub_dependency(item)
 
     def reset(self):
         """

dockermap/map/policy/dep.py
@@ -34,7 +34,8 @@ class ContainerDependencyResolver(MultiDependencyResolver):
         :raise CircularDependency: If the current element depends on one found deeper in the hierarchy.
         """
         dep = []
-        for parent_dep in chain.from_iterable(map(resolve_parent, parents)):
+        for parent in parents:
+            parent_dep = resolve_parent(parent)
             if parent_dep:
                 new_dep = [p for p in parent_dep if p not in dep]
                 dep.extend(new_dep)

tests/test_dependency_resolution.py
@@ -1,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+
+from dockermap.map.container import ContainerMap
+from dockermap.map.policy.dep import ContainerDependencyResolver
+
+
+TEST_MAP_DATA = {
+    'a': dict(uses=['b', 'c']),
+    'b': dict(uses=['dv', 'f'], links='e.1'),
+    'd': dict(uses='e', instances=['1', '2'], attaches='dv'),
+    'e': dict(instances=['1', '2']),
+    'f': dict(),
+    'x': dict(uses=['b', 'f']),
+}
+
+
+class ContainerDependencyTest(unittest.TestCase):
+    def setUp(self):
+        test_map = ContainerMap('test_map', initial=TEST_MAP_DATA, check_integrity=False)
+        self.res = ContainerDependencyResolver(test_map)
+
+    def assertOrder(self, dependency_list, *items):
+        iterator = iter(items)
+        last_item = iterator.next()
+        last_idx = dependency_list.index(last_item)
+        for item in iterator:
+            index = dependency_list.index(item)
+            if index < last_idx:
+                self.fail("{0} found before {1}, should be later.".format(item, last_item))
+
+    def test_resolution_order(self):
+        a_dep = self.res.get_container_dependencies('test_map', 'a')
+        self.assertOrder(a_dep,
+                         ('test_map', 'd', None),
+                         ('test_map', 'b', None))
+        self.assertOrder(a_dep,
+                         ('test_map', 'e', None),
+                         ('test_map', 'd', None))
+        self.assertOrder(a_dep,
+                         ('test_map', 'e', '1'),
+                         ('test_map', 'b', None))
+        self.assertOrder(a_dep,
+                         ('test_map', 'f', None),
+                         ('test_map', 'b', None))
+        x_dep = self.res.get_container_dependencies('test_map', 'x')
+        self.assertOrder(x_dep,
+                         ('test_map', 'f', None),
+                         ('test_map', 'b', None))
+
+
+if __name__ == '__main__':
+    unittest.main()