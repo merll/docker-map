815f2f8b4f72f8ba3b62eb3db57d3f8aeb68cd24
Matthias Erll <matthias@erll.de>
2016-04-04 17:56:43 +0200

    Fixed image name.

--- dockermap/__init__.py
+++ dockermap/__init__.py
@@ -5,4 +5,4 @@ from __future__ import unicode_literals
 __version__ = '0.6.6b1'
 
 DEFAULT_BASEIMAGE = 'tianon/true:latest'
-DEFAULT_COREIMAGE = '/busybox:latest'
+DEFAULT_COREIMAGE = 'busybox:latest'
