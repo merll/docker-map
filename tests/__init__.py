d215ce6eb5256182d15da2e57e4a5d5a115c1066
Matthias Erll <matthias@erll.de>
2016-08-24 15:37:28 +0200

    Use absolute imports in tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
 MAP_DATA_1 = {
     'repository': 'registry.example.com',
