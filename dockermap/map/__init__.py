3ff87823f9094edf862d527bba92cf9769e5abff
Matthias Erll <matthias@erll.de>
2017-01-04 23:35:51 +0100

    Completed copy of DictMap objects.

--- dockermap/map/__init__.py
+++ dockermap/map/__init__.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
+import itertools
 import six
 
+from ..utils import merge_list
 
-def _update_instance(instance, obj_dict):
+
+def _update_instance_from_dict(instance, obj_dict):
     if not obj_dict:
         return
-    for p_name in instance.__class__.core_properties:
+    for p_name in instance.__class__.external_properties:
         if p_name in obj_dict:
             object.__setattr__(instance, p_name, obj_dict.pop(p_name))
+
+
+def _update_instance_from_obj(instance, obj):
+    for p_name in instance.__class__.internal_properties:
+        object.__setattr__(instance, p_name, getattr(obj, p_name))
 
 
 class PropertyDictMeta(type):
     def __init__(cls, name, bases, dct):
         cls.core_properties = cp = [d_name for d_name, d_type in six.iteritems(dct) if isinstance(d_type, property)]
-        cp.extend('_{0}'.format(d_name) for d_name in cp[:])
-        cp_set = set(cp)
-        cp_add = cp_set.add
-        for base in bases:
-            if hasattr(base, 'core_properties'):
-                cp.extend(b_cp for b_cp in base.core_properties if b_cp not in cp_set and not cp_add(b_cp))
+        cls.external_properties = ce = cp[:]
+        cls.internal_properties = ci = ['_{0}'.format(d_name) for d_name in cp]
+        cp.extend(ci)
+        cp_bases = [base for base in bases if hasattr(base, 'core_properties')]
+        merge_list(cp, itertools.chain.from_iterable(base.core_properties for base in cp_bases))
+        merge_list(ce, itertools.chain.from_iterable(base.external_properties for base in cp_bases))
+        merge_list(ci, itertools.chain.from_iterable(base.internal_properties for base in cp_bases))
         super(PropertyDictMeta, cls).__init__(name, bases, dct)
 
 
@@ -29,7 +38,7 @@ class AttributeMixin(six.with_metaclass(PropertyDictMeta)):
     return keys and values.
     """
     def __init__(self, *args, **kwargs):
-        _update_instance(self, kwargs)
+        _update_instance_from_dict(self, kwargs)
         super(AttributeMixin, self).__init__(*args, **kwargs)
 
     def __getattr__(self, item):
@@ -53,20 +62,25 @@ class AttributeMixin(six.with_metaclass(PropertyDictMeta)):
     def update(self, other=None, **kwargs):
         if other is not None:
             if isinstance(other, self.__class__):
-                for p in self.__class__.core_properties:
-                    object.__setattr__(self, p, getattr(other, p))
+                _update_instance_from_obj(self, other)
             elif isinstance(other, dict):
                 other = other.copy()
-                _update_instance(self, other)
+                _update_instance_from_dict(self, other)
             else:
                 raise ValueError("Expected {0} or dictionary; found '{1}'".format(type(self).__name__, type(other).__name__))
-        _update_instance(self, kwargs)
+        _update_instance_from_dict(self, kwargs)
         super(AttributeMixin, self).update(other, **kwargs)
 
 
 class DictMap(AttributeMixin, dict):
-    pass
+    def copy(self):
+        new_instance = self.__class__(self)
+        _update_instance_from_obj(new_instance, self)
+        return new_instance
 
 
 class DefaultDictMap(AttributeMixin, defaultdict):
-    pass
+    def copy(self):
+        new_instance = self.__class__(self.default_factory, self)
+        _update_instance_from_obj(new_instance, self)
+        return new_instance
--- dockermap/utils.py
+++ dockermap/utils.py
@@ -8,3 +8,25 @@ from .functional import lazy_once
 
 expand_path = lambda value: os.path.expanduser(os.path.expandvars(value))
 expand_path_lazy = lambda value: lazy_once(expand_path, value)
+
+
+def merge_list(merged_list, items):
+    """
+    Merges items into a list, appends ignoring duplicates but retaining the original order. This modifies the list and
+    does not return anything.
+
+    :param merged_list: List to append de-duplicated items to.
+    :type merged_list: list
+    :param items: Items to merge into the list.
+    :type items: collections.Iterable
+    """
+    if not items:
+        return
+    if not merged_list:
+        merged_list.extend(items)
+        return
+    merged_set = set(merged_list)
+    merged_add = merged_set.add
+    merged_list.extend(item
+                       for item in items
+                       if item not in merged_set or merged_add(item))
