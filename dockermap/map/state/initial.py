f3f021f14f7504700c09f18c2073881973ca4447
Matthias Erll <matthias@erll.de>
2017-08-05 14:45:24 +0200

    Simplified initial state.

--- dockermap/map/state/initial.py
+++ dockermap/map/state/initial.py
@@ -2,25 +2,12 @@
 from __future__ import unicode_literals
 
 from . import State, StateFlags
-from .base import DependencyStateGenerator, ContainerBaseState, NetworkBaseState
+from .base import DependencyStateGenerator, AbstractState
 
 
-class InitialContainerState(ContainerBaseState):
+class InitialState(AbstractState):
     """
-    Assumes every container to be absent. This is intended for testing and situations where the actual state
-    cannot be determined.
-    """
-    def inspect(self):
-        # No need to actually make any client call
-        pass
-
-    def get_state(self):
-        return State.ABSENT, StateFlags.NONE, {}
-
-
-class InitialNetworkState(NetworkBaseState):
-    """
-    Assumes every network to be absent. This is intended for testing and situations where the actual state
+    Assumes every item to be absent. This is intended for testing and situations where the actual state
     cannot be determined.
     """
     def inspect(self):
@@ -32,5 +19,5 @@ class InitialNetworkState(NetworkBaseState):
 
 
 class InitialStateGenerator(DependencyStateGenerator):
-    container_state_class = InitialContainerState
-    network_state_class = InitialNetworkState
+    container_state_class = InitialState
+    network_state_class = InitialState
