3cfc5cb0b7d08e1b756e0c9ed3ee4be97b77a227
Matthias Erll <matthias@erll.de>
2017-08-10 08:52:03 +0200

    Updated test.

--- tests/test_state.py
+++ tests/test_state.py
@@ -712,7 +712,10 @@ class TestPolicyStateGenerators(unittest.TestCase):
             self.assertEqual(server_state.base_state, State.RUNNING)
             self.assertEqual(server_state.state_flags & StateFlags.NEEDS_RESET, 0)
             self.assertEqual(server_state.state_flags & StateFlags.EXEC_COMMANDS, StateFlags.EXEC_COMMANDS)
-            self.assertDictEqual(server_state.extra_data, {'exec_commands': [cmd3]})
+            self.assertDictEqual(server_state.extra_data, {
+                'exec_commands': [cmd3],
+                'id': get_container_id('{0}.server'.format(self.map_name, 'server')
+            })
 
 
 class TestPolicyStateUtils(unittest.TestCase):
