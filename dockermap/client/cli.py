0bb401d2fb99b59a003c628ae6b8f421e8247b56
Matthias Erll <matthias@erll.de>
2017-08-24 13:46:47 +0200

    Implemented networks in CLI generator.

--- dockermap/client/cli.py
+++ dockermap/client/cli.py
@@ -20,6 +20,11 @@ KWARG_MAP = {
     'extra_hosts': 'add-host',
     'nocache': 'no-cache',
     'volumes': 'volume',
+    'options': 'opt',
+    'ipv4_address': 'ip',
+    'ipv6_address': 'ip6',
+    'links': 'link',
+    'aliases': 'alias',
 }
 NONE_TAG = '<none>'
 _arg_format = '--{0}={1}'.format
@@ -80,6 +85,16 @@ def _container_info(line):
     }
 
 
+def _network_info(line):
+    items = line.split()
+    return {
+        'Id': items[0],
+        'Name': items[1],
+        'Driver': items[2],
+        'Scope': items[3],
+    }
+
+
 def _first_key_value(d, *keys):
     for k in keys:
         v = d.get(k)
@@ -108,6 +123,7 @@ def _transform_kwargs(ka):
 def _transform_create_kwargs(ka):
     network = ka.pop('network_mode', None)
     network_disabled = ka.pop('network_disabled', False)
+    network_config = ka.pop('networking_config', None)
     environment = ka.pop('environment', None)
     binds = ka.pop('binds', None)
     volumes = ka.pop('volumes', None) if binds else None
@@ -130,6 +146,25 @@ def _transform_create_kwargs(ka):
         yield _arg_format('net', 'none')
     elif network:
         yield _arg_format('net', network)
+    elif network_config:
+        ep_config = network_config['EndpointsConfig']
+        network_name = ep_config.keys()[0]
+        nc = ep_config[network_name]
+        yield _arg_format('net', network_name)
+        for a in nc.get('Aliases') or ():
+            yield _arg_format('network-alias', a)
+        for l in nc.get('Links') or ():
+            yield _arg_format(l)
+        ipam = nc.get('IPAMConfig')
+        if ipam:
+            ip4 = ipam.get('IPv4Address')
+            if ip4:
+                yield _arg_format('ip', ip4)
+            ip6 = ipam.get('IPv6Address')
+            if ip6:
+                yield _arg_format('ip6', ip6)
+            for l in ipam.get('LinkLocalIPs') or ():
+                yield _arg_format('link-local-ip', l)
 
     if volumes:
         bind_keys = {}
@@ -183,20 +218,38 @@ def parse_containers_output(out):
     ]
 
 
-def parse_inspect_output(out):
+def parse_networks_output(out):
     """
-    Parses the output of the Docker CLI 'docker inspect <container>'. Essentially just returns the parsed JSON string,
-    like the Docker API does.
+    Parses the output of the Docker CLI 'docker network ls' and returns it in the format similar to the Docker API.
 
     :param out: CLI output.
     :type out: unicode | str
+    :return: Parsed result.
+    :rtype: list[dict]
+    """
+    if not out:
+        return []
+    line_iter = iter(out.splitlines())
+    next(line_iter)  # Skip header
+    return [_network_info(line) for line in line_iter]
+
+
+def parse_inspect_output(out, item_type):
+    """
+    Parses the output of the Docker CLI 'docker inspect <container>' or 'docker network inspect <network>'. Essentially
+    just returns the parsed JSON string, like the Docker API does.
+
+    :param out: CLI output.
+    :type out: unicode | str
+    :param item_type: Type of the item that has been inspected (e.g. 'container').
+    :type item_type: unicode | str
     :return: Parsed result.
     :rtype: dict
     """
     parsed = json.loads(out, encoding='utf-8')
     if parsed:
         return parsed[0]
-    raise NotFound("Container not found.", None)
+    raise NotFound("{0} not found.".format(item_type.title()), None)
 
 
 def parse_images_output(out):
@@ -259,6 +312,14 @@ def parse_top_output(out):
     }
 
 
+def _extend_or_append(cmd_args, exec_cmd):
+    if exec_cmd:
+        if isinstance(exec_cmd, list):
+            cmd_args.extend(exec_cmd)
+        else:
+            cmd_args.append(exec_cmd)
+
+
 class DockerCommandLineOutput(object):
     cmd_map = {
         'create_container': 'create',
@@ -268,6 +329,12 @@ class DockerCommandLineOutput(object):
         'inspect_container': 'inspect',
         'remove_container': 'rm',
         'remove_image': 'rmi',
+        'create_network': 'network create',
+        'networks': 'network ls',
+        'inspect_network': 'network inspect',
+        'remove_network': 'network rm',
+        'connect_container_to_network': 'network connect',
+        'disconnect_container_from_network': 'network disconnect',
     }
 
     def __init__(self, cmd_prefix=None, default_bin='docker', cmd_args=None):
@@ -288,37 +355,43 @@ class DockerCommandLineOutput(object):
         cmd_prefix = None
         cmd_args = [cli_cmd]
         if cli_cmd == 'create':
-            p_arg = kwargs.pop('image')
-            exec_cmd = kwargs.pop('command', None)
+            p_args = [kwargs.pop('image')]
+            _extend_or_append(p_args, kwargs.pop('command', None))
             cmd_args.extend(_transform_create_kwargs(kwargs))
         elif cli_cmd == 'exec':
-            p_arg = kwargs.pop('container')
+            p_args = [kwargs.pop('container')]
+            _extend_or_append(p_args, kwargs.pop('cmd'))
             cmd_args.append('--detach')
-            exec_cmd = kwargs.pop('cmd')
+            cmd_args.extend(_transform_kwargs(kwargs))
+        elif cli_cmd == 'network create':
+            p_args = [kwargs.pop('name')]
+            cmd_args.extend(_transform_kwargs(kwargs))
+        elif cli_cmd.startswith('network') and (cli_cmd.endswith('connect') or cli_cmd.endswith('rm')):
+            p_args = [kwargs.pop('net_id')]
+            if cli_cmd.endswith('connect'):
+                p_args.append(kwargs.pop('container'))
             cmd_args.extend(_transform_kwargs(kwargs))
         else:
-            if cli_cmd in ('images', 'ps'):
+            if cli_cmd in ('images', 'ps', 'network ls'):
                 cmd_args.append('--no-trunc')
                 if cli_cmd == 'ps':
                     cmd_args.append(CONTAINER_FORMAT_ARG)
-                p_arg = None
+                p_args = None
             elif cli_cmd == 'version':
                 cmd_args.append(VERSION_FORMAT_ARG)
-                p_arg = None
+                p_args = None
             else:
                 if cli_cmd == 'wait':
                     timeout = kwargs.pop('timeout', None)
                     if timeout:
                         cmd_prefix = 'timeout -s INT {0} '.format(timeout)
-                p_arg = kwargs.pop('container', None)
-            exec_cmd = None
+                container_arg = kwargs.pop('container', None)
+                if container_arg:
+                    p_args = [container_arg]
+                else:
+                    p_args = None
             cmd_args.extend(_transform_kwargs(kwargs))
-        if p_arg:
-            cmd_args.append(p_arg)
-        if exec_cmd:
-            if isinstance(exec_cmd, list):
-                cmd_args.extend(exec_cmd)
-            else:
-                cmd_args.append(exec_cmd)
+        if p_args:
+            cmd_args.extend(p_args)
         cmd_args.extend(args)
         return '{0}{1} {2}'.format(cmd_prefix or '', self._cmd, ' '.join(cmd_args))
