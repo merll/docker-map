e14fe86a6ff5c33d121eb2f9157e9359cb80dd02
Matthias Erll <matthias@erll.de>
2018-01-15 07:54:29 +0100

    Updated convenience imports.

--- dockermap/api.py
+++ dockermap/api.py
@@ -7,10 +7,11 @@ from .client.base import DockerClientWrapper
 from .exceptions import PartialResultsError, DockerStatusError
 from .map.client import MappingDockerClient
 from .map.config.client import ClientConfiguration, USE_HC_MERGE
-from .map.config.host_volume import HostVolumeConfiguration
 from .map.config.container import ContainerConfiguration
-from .map.config.network import NetworkConfiguration
+from .map.config.host_volume import HostVolumeConfiguration
 from .map.config.main import ContainerMap
+from .map.config.network import NetworkConfiguration
+from .map.config.volume import VolumeConfiguration
 from .map.exceptions import ActionRunnerException, MapIntegrityError, ScriptActionException, ScriptRunException
 from .map.input import (ContainerLink, ExecPolicy, ExecCommand, ItemType, MapConfigId, NetworkEndpoint, PortBinding,
-                        SharedVolume)
+                        SharedVolume, HealthCheck)
